33e73b94bf2ce437f32b19d11e0523e97723f239
Demanrusss <deman.russs@gmail.com>
2024-04-14 23:53:27 +0500

    Prop tutned to nullable for razorPages. Db Updated with new data

--- ManageDb/Models/TNVEDCode.cs
+++ ManageDb/Models/TNVEDCode.cs
@@ -8,6 +8,6 @@ namespace ManageDb.Models
         [RegularExpression(@"[0-9]{10}", ErrorMessage = "Код ТН ВЭД должен состоять из 10 цифр")]
         public string Code { get; set; } = null!;
         public string Name { get; set; } = null!;
-        public virtual ICollection<TechReg> TechRegs { get; set; } = new HashSet<TechReg>();
+        public virtual ICollection<TechReg>? TechRegs { get; set; } = new HashSet<TechReg>();
     }
 }
