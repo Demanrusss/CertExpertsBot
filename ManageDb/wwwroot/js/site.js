9e77fa1d021c55fb6b7935e573a95e416106fbd8
Demanrusss <deman.russs@gmail.com>
2024-06-26 01:45:36 +0500

    Db updated (88/97). Adjusted js-script on CreateForm

--- ManageDb/wwwroot/js/site.js
+++ ManageDb/wwwroot/js/site.js
@@ -1,3 +1,21 @@
-﻿function trim(element) {
+﻿function toUpperCaseFirstLetter(element) {
+    if (element !== '') {
+        element.value = element.value[0].toUpperCase() + element.value.slice(1);
+    }
+};
+
+function toRightSymbols(element) {
+    if (element !== '') {
+        element.value = element.value.replaceAll('см?', 'см3');
+    }
+};
+
+function trim(element) {
     element.value = element.value.trim();
 };
+
+function noDummiesEntrance(element) {
+    trim(element);
+    toUpperCaseFirstLetter(element);
+    toRightSymbols(element);
+}
