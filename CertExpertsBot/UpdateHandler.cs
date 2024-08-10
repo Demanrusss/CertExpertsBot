344f2867cb436c3fb90688ae14b3483191deca78
Demanrusss <deman.russs@gmail.com>
2024-08-10 13:11:47 +0500

    Refactoring

--- CertExpertsBot/UpdateHandler.cs
+++ CertExpertsBot/UpdateHandler.cs
@@ -1,10 +1,7 @@
-﻿using CertExpertsBot.Data;
-using CertExpertsBot.UpdateTypeHandlers;
-using Microsoft.EntityFrameworkCore;
+﻿using CertExpertsBot.UpdateTypeHandlers;
 using Telegram.Bot;
 using Telegram.Bot.Types;
 using Telegram.Bot.Types.Enums;
-using Telegram.Bot.Types.ReplyMarkups;
 
 namespace CertExpertsBot
 {
@@ -13,46 +10,13 @@ namespace CertExpertsBot
         public static async Task HandleUpdateAsync(ITelegramBotClient botClient, 
             Update update, CancellationToken cancellationToken)
         {
-            if (botClient == null || update == null)
-                return;
-
             switch (update.Type)
             {
                 case UpdateType.Message:
-                    var message = update.Message;
-                    var response = MessageHandler.Response(message);
-                    if (response.Contains("Обработчик кодов ТНВЭД"))
-                    {
-                        await botClient.SendTextMessageAsync(chatId: message.Chat,
-                                                             text: response,
-                                                             cancellationToken: cancellationToken);
-                        var replyMarkupDefault = CallbackQueryAnswer.ReplyMarkupKB_AllNumbers();
-                        await botClient.SendTextMessageAsync(chatId: message.Chat,
-                                                             text: ".",
-                                                             replyMarkup: replyMarkupDefault,
-                                                             cancellationToken: cancellationToken);
-                    }
-                    else
-                        await botClient.SendTextMessageAsync(chatId: message.Chat,
-                                                             text: response,
-                                                             cancellationToken: cancellationToken);
+                    await HandleMessageAsync(botClient, update.Message, cancellationToken);
                     break;
                 case UpdateType.CallbackQuery:
-                    var callbackQuery = update.CallbackQuery;
-                    var replyMarkup = CallbackQueryAnswer.ReplyMarkup(callbackQuery);
-                    var text = callbackQuery.Message.Text + callbackQuery.Data;
-                    if (text.Length == 11)
-                    {
-                        response = MessageHandler.Response(new Message() { Text = text });
-                        await botClient.SendTextMessageAsync(callbackQuery.Message.Chat,
-                                                             text: response,
-                                                             cancellationToken: cancellationToken);
-                    }
-                    else
-                        await botClient.EditMessageTextAsync(callbackQuery.Message.Chat,
-                                                             callbackQuery.Message.MessageId,
-                                                             text, 
-                                                             replyMarkup: replyMarkup);
+                    await HandleCallbackQueryAsync(botClient, update.CallbackQuery, cancellationToken);
                     break;
                 default:
                     Console.WriteLine("New Type was found. " + update.Type.ToString());
@@ -60,80 +24,55 @@ namespace CertExpertsBot
             }
         }
 
+        private static async Task HandleMessageAsync(ITelegramBotClient botClient, Message? message, 
+            CancellationToken cancellationToken)
+        {
+            if (message == null)
+                return;
+            string response = MessageHandler.Response(message);
+            if (response.Contains("Обработчик кодов ТНВЭД"))
+            {
+                await botClient.SendTextMessageAsync(chatId: message.Chat,
+                text: response,
+                                                     cancellationToken: cancellationToken);
+                var replyMarkupDefault = CallbackQueryHandler.ReplyMarkupKB_AllNumbers();
+                await botClient.SendTextMessageAsync(chatId: message.Chat,
+                                                     text: ".",
+                replyMarkup: replyMarkupDefault,
+                                                     cancellationToken: cancellationToken);
+            }
+            else
+                await botClient.SendTextMessageAsync(chatId: message.Chat,
+                                                     text: response,
+                                                     cancellationToken: cancellationToken);
+        }
+
+        private static async Task HandleCallbackQueryAsync(ITelegramBotClient botClient, CallbackQuery? callbackQuery,
+            CancellationToken cancellationToken)
+        {
+            if (callbackQuery == null || callbackQuery.Message == null)
+                return;
+            var replyMarkup = CallbackQueryHandler.ReplyMarkup_Numbers(callbackQuery);
+            var text = callbackQuery.Message.Text + callbackQuery.Data;
+            if (text.Length == 11)
+            {
+                var response = MessageHandler.Response(new Message() { Text = text });
+                await botClient.SendTextMessageAsync(callbackQuery.Message.Chat,
+                text: response,
+                                                     cancellationToken: cancellationToken);
+            }
+            else
+                await botClient.EditMessageTextAsync(callbackQuery.Message.Chat,
+                                                     callbackQuery.Message.MessageId,
+                                                     text,
+                                                     replyMarkup: replyMarkup);
+        }
+
         public static async Task HandlePollingErrorAsync(ITelegramBotClient botClient, 
             Exception exception, CancellationToken cancellationToken)
         {
             Console.Error.WriteLine(exception);
             await Task.CompletedTask;
-        }
-    }
-
-    public static class CallbackQueryAnswer
-    {
-        private static readonly AppDbContext dbContext = new AppDbContext();
-
-        public static InlineKeyboardMarkup ReplyMarkup(CallbackQuery callbackQuery)
-        {
-            switch (callbackQuery.Data)
-            {
-                case "0":
-                case "1":
-                case "2":
-                case "3":
-                case "4":
-                case "5":
-                case "6":
-                case "7":
-                case "8":
-                case "9":
-                    var partCode = callbackQuery.Message.Text.Substring(1) + callbackQuery.Data;
-                    var availableNumbers = AvailableNumbers(partCode);
-                    return ReplyMarkupKB_AvailableNumbers(availableNumbers);
-                default:
-                    return ReplyMarkupKB_AllNumbers();
-            }
-        }
-
-        private static IList<string> AvailableNumbers(string partCode)
-        {
-            var availableNumbers = new List<string>();
-
-            for (int i = 0; i <= 9; i++)
-            {
-                bool newPartCodeExists = dbContext.TNVEDCodes.Any(c => c.Code.StartsWith(partCode + i.ToString()));
-                if (newPartCodeExists)
-                    availableNumbers.Add(i.ToString());
-            }
-
-            return availableNumbers;
-        }
-
-        private static InlineKeyboardMarkup ReplyMarkupKB_AvailableNumbers(IList<string> availableNumbers)
-        {
-            var keyBoard = new List<List<InlineKeyboardButton>>();
-
-            var row1 = new List<InlineKeyboardButton>();
-            var row2 = new List<InlineKeyboardButton>();
-            for (int i = 0; i < availableNumbers.Count; i++)
-            {
-                if (i <= 7)
-                    row1.Add(InlineKeyboardButton.WithCallbackData(availableNumbers[i], availableNumbers[i]));
-                else
-                    row2.Add(InlineKeyboardButton.WithCallbackData(availableNumbers[i], availableNumbers[i]));
-            }
-
-            keyBoard.Add(row1);
-            if (row2.Count > 0)
-                keyBoard.Add(row2);
-
-            return new InlineKeyboardMarkup(keyBoard);
-        }
-
-        public static InlineKeyboardMarkup ReplyMarkupKB_AllNumbers()
-        {
-            var allNumbers = new List<string>() { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9" };
-
-            return ReplyMarkupKB_AvailableNumbers(allNumbers);
         }
     }
 }
---
+++ CertExpertsBot/UpdateTypeHandlers/CallbackQueryHandler.cs
@@ -0,0 +1,79 @@
+﻿using CertExpertsBot.Data;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.ReplyMarkups;
+
+namespace CertExpertsBot.UpdateTypeHandlers
+{
+    public static class CallbackQueryHandler
+    {
+        private static readonly AppDbContext dbContext = new AppDbContext();
+
+        public static InlineKeyboardMarkup ReplyMarkup_Numbers(CallbackQuery callbackQuery)
+        {
+            if (callbackQuery.Message == null || callbackQuery.Message.Text == null)
+                return ReplyMarkupKB_AllNumbers();
+
+            switch (callbackQuery.Data)
+            {
+                case "0":
+                case "1":
+                case "2":
+                case "3":
+                case "4":
+                case "5":
+                case "6":
+                case "7":
+                case "8":
+                case "9":
+                    var partCode = callbackQuery.Message.Text.Substring(1) + callbackQuery.Data;
+                    var availableNumbers = AvailableNumbers(partCode);
+                    return ReplyMarkupKB_AvailableNumbers(availableNumbers);
+                default:
+                    return ReplyMarkupKB_AllNumbers();
+            }
+        }
+
+        private static IList<string> AvailableNumbers(string partCode)
+        {
+            var availableNumbers = new List<string>();
+
+            bool newPartCodeExists;
+            for (int i = 0; i <= 9; i++)
+            {
+                newPartCodeExists = dbContext.TNVEDCodes.Any(c => c.Code.StartsWith(partCode + i.ToString()));
+                if (newPartCodeExists)
+                    availableNumbers.Add(i.ToString());
+            }
+
+            return availableNumbers;
+        }
+
+        private static InlineKeyboardMarkup ReplyMarkupKB_AvailableNumbers(IList<string> availableNumbers)
+        {
+            var keyBoard = new List<List<InlineKeyboardButton>>();
+
+            var row1 = new List<InlineKeyboardButton>();
+            var row2 = new List<InlineKeyboardButton>();
+            for (int i = 0; i < availableNumbers.Count; i++)
+            {
+                if (i <= 4)
+                    row1.Add(InlineKeyboardButton.WithCallbackData(availableNumbers[i], availableNumbers[i]));
+                else
+                    row2.Add(InlineKeyboardButton.WithCallbackData(availableNumbers[i], availableNumbers[i]));
+            }
+
+            keyBoard.Add(row1);
+            if (row2.Count > 0)
+                keyBoard.Add(row2);
+
+            return new InlineKeyboardMarkup(keyBoard);
+        }
+
+        public static InlineKeyboardMarkup ReplyMarkupKB_AllNumbers()
+        {
+            var allNumbers = new List<string>() { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+
+            return ReplyMarkupKB_AvailableNumbers(allNumbers);
+        }
+    }
+}
